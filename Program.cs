fb65bb3a95592200b28c5460cf9fe886a5cceb15
Bear <lagerblad.bjorn@gmail.com>
2023-10-02 14:15:22 +0200

    påbörjar rapporter

--- Program.cs
+++ Program.cs
@@ -1,25 +1,38 @@
 ﻿namespace GrupparbetePolisen1984;
 
-class Program
+public class Uttryckingar
 {
     static void Main(string[] args)
-    
     {
-       List<string> Typ = new List<string>();
-            Console.WriteLine("Ange typ av brott:");
-            Typ.Add(Console.ReadLine()); 
-        
-        List<string> Plats = new List<string>();
-            Console.WriteLine("Ange plats:");
-            Plats.Add(Console.ReadLine()); 
+        System.Console.WriteLine("Ange typ av brott:");
+        string Typ = Console.ReadLine(); 
 
-        List<double> Tid = new List<double>();
-            Console.WriteLine("Ange tid:");
-            string input = Console.ReadLine();
+        System.Console.WriteLine("Ange plats:");
+        string Plats = Console.ReadLine(); 
 
-         List<string> Poliser = new List<string>();
-            Console.WriteLine("Ange vilka poliser som deltog:");
-            Poliser.Add(Console.ReadLine()); 
+        System.Console.WriteLine("Ange tid:");
+        double Tid = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.WriteLine("Ange vilka poliser som deltog:");
+        string Poliser = Console.ReadLine(); 
         
+        System.Console.WriteLine(Typ + "\n" + Plats + "\n" + Tid + "\n" + Poliser);
+
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("Ange typ av brott:");
+        string Typ = Console.ReadLine(); 
+
+        System.Console.WriteLine("Ange plats:");
+        string Plats = Console.ReadLine(); 
+
+        System.Console.WriteLine("Ange tid:");
+        double Tid = Convert.ToDouble(Console.ReadLine());
+
+        System.Console.WriteLine("Ange vilka poliser som deltog:");
+        string Poliser = Console.ReadLine(); 
+        
+        System.Console.WriteLine(Typ + "\n" + Plats + "\n" + Tid + "\n" + Poliser);
+
     }
 }
