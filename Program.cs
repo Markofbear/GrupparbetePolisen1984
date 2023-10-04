4ed1fb60c43a49db66721bc1010dc92a7960552d
Markofbear <lagerblad.bjorn@gmail.com>
2023-10-04 10:12:50 +0200

    lagt till tid och klasser

--- Program.cs
+++ Program.cs
@@ -1,37 +1,130 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
 
-namespace GrupparbetePolisen1984;
-
-public class Polis
+namespace GrupparbetePolisen1984
 {
-    static void Main(string[] args)
-    { 
-        int val; //test
-        switch(val)
+    class Program
+    {
+        static void Main(string[] args)
         {
+            List<string> brottrapport = new List<string>(); // Startar en lista som sparar registrering
+            List<string> brottrapport2 = new List<string>(); // Startar en lista för rapporter från case 2
 
-        System.Console.WriteLine("1: Registrering av uttryckningar \n 2: Rapporter \n 3:");
+            while (true)
+            {
+                int val;
 
-        case 1:
+                Console.WriteLine("1: Registrering av uttryckningar");
+                Console.WriteLine("2: Rapporter");
+                Console.WriteLine("4: Information???");
+                Console.WriteLine("0: Exit");
 
-        System.Console.WriteLine("Ange typ av brott:");
-        string Typ = Console.ReadLine(); 
+                if (int.TryParse(Console.ReadLine(), out val)) // Om valet är en annan än siffran skicka till else
+                {
+                    switch (val)
+                    {
+                        case 1:
+                            Case1.Registrering(brottrapport);
+                            break;
 
-        System.Console.WriteLine("Ange plats:");
-        string Plats = Console.ReadLine(); 
+                        case 2:
+                            Case2.Rapporter(brottrapport2);
+                            break;
 
-        System.Console.WriteLine("Ange tid:");
-        double Tid = Convert.ToDouble(Console.ReadLine());
+                        case 4:
+                            Case4.VisaRapporter(brottrapport, brottrapport2);
+                            break;
 
-        System.Console.WriteLine("Ange vilka poliser som deltog:");
-        string Poliser = Console.ReadLine(); 
-        
-        System.Console.WriteLine(Typ + "\n" + Plats + "\n" + Tid + "\n" + Poliser);
-        break;
+                        case 0:
+                            return;
 
-        case 2:
+                        default:
+                            Console.WriteLine("ERRORTEST");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERRORTESTÄRDETENFÖRMYCKET??");
+                }
+            }
+        }
+    }
 
-    
+    static class Case1
+    {
+        public static void Registrering(List<string> brottrapport)
+        {
+            Console.WriteLine("Ange typ av brott:");
+            string Typ = Console.ReadLine();
+
+            Console.WriteLine("Ange plats:");
+            string Plats = Console.ReadLine();
+
+            Console.WriteLine("Ange tid:");
+            double Tid;
+            if (double.TryParse(Console.ReadLine(), out Tid))
+            {
+                Console.WriteLine("Ange vilka poliser som deltog:");
+                string Poliser = Console.ReadLine();
+
+                string rapport = $"{Typ}\n{Plats}\n{Tid}\n{Poliser}\n"; // Skapar en rapport 
+                brottrapport.Add(rapport); // Sparar rapport i listan
+            }
+            else
+            {
+                Console.WriteLine("Ej en tidpunkt! Try again.");
+            }
+        }
+    }
+
+    static class Case2
+    {
+        public static void Rapporter(List<string> brottrapport2)
+        {
+            Console.WriteLine("Ange Rapportnummer:");
+            double rapportnr;
+            if (!double.TryParse(Console.ReadLine(), out rapportnr))
+            {
+                Console.WriteLine("Ej ett rapportnummer försök igen!");
+                return;
+            }
+
+            Console.WriteLine("Ange datum:");
+            double datum;
+            if (!double.TryParse(Console.ReadLine(), out datum))
+            {
+                Console.WriteLine("Ej ett datum försök igen!");
+                return;
+            }
+
+            Console.WriteLine("Ange polistation:");
+            string polisstation = Console.ReadLine();
+
+            Console.WriteLine("Beskriv vad som hände kortfattat:");
+            string beskrivning = Console.ReadLine();
+
+            string rapport2 = $"{rapportnr}\n{datum}\n{polisstation}\n{beskrivning}"; // Skapar en rapport 
+            brottrapport2.Add(rapport2); // Sparar rapport i den andra listan
+        }
+    }
+
+    static class Case4
+    {
+        public static void VisaRapporter(List<string> brottrapport, List<string> brottrapport2)
+        {
+            Console.WriteLine("Rapporter från case 1:");
+            foreach (string rapport in brottrapport)
+            {
+                Console.WriteLine(rapport);
+            }
+
+            Console.WriteLine("Rapporter från case 2:");
+            foreach (string rapport in brottrapport2)
+            {
+                Console.WriteLine(rapport);
+            }
         }
     }
 }
+
